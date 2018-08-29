df297c679d953ab006ac1557596c0bb47591fb83
edya123 <42386022+edya123@users.noreply.github.com>
2018-08-29 19:36:43 +0300

    Initial commit

---
+++ homework_7/js/main.js
@@ -0,0 +1,15 @@
+// ClickCoin clicker game.
+
+// 1. Spēles lauki:
+// 1.1 Kopējais ClickCoin skaits, kas ir savākts.
+// 1.2 Pieejamais ClickCoin skaits, ko var tērēt.
+// 1.3. Poga "Click Me", kas palielinās ClickCoin skaitu
+// 1.4. Pogas, lai nopirktu 1x un 10x upgrades.
+// 1.5. Katram upgrades tipam jāparāda cik tādi jau iegādāti.
+
+// 2. Spēles gaita:
+// 2.1. Uzpiežot Click Me pogu, palielinās ClickCoin skaits par 1.
+// 2.1. Kad ir savākti vismaz 10 ClickCoin, tad var nopirkt pirmo upgrade(1x). Sākotnēji tas maksā 10 ClickCoin, bet ar katru nopirkšanas reizi cena palielinās par 10%.
+// 2.3. Kad ir savākti vismaz 90 ClickCoin, tad var nopirkt otro upgrade (10x). Cena paceļās tā pat kā 1x Upgrade.
+// 2.4. 1x Upgrade dod 1 ClickCoin katru sekundi. 10x upgrade dod 10 koinus katru sekundi
+// 2.5. Ja nepietiek ClickCoin, lai nopirktu kādu upgrade, bogai ir jābūt deaktivizētai (disabled)
